95591cfad570b0b3306ba60e366a22a72a9bb487
Vítor Johansen Guerra <vitorjguerra@hotmail.com>
2023-04-18 18:55:04 -0300

    melhorias no css,java script e implementacao das lis

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,18 @@ element.addEventListener("click", trocarMensagem);
 function trocarMensagem() {
   document.getElementById("btnDuvidas").innerHTML = "Eu também tenho!";
 }
+
+
+function irParaOutraPagina() {
+  window.location.href = 'builds-pve.html';
+}
+
+
+const elemento = document.getElementById('listaDeClasses');
+const lista = document.getElementByClassName('minha-lista');
+
+elemento.addEventListener('mouseover', mostrarListaDeBuilds);
+
+function mostrarListaDeBuilds() {
+  lista.style.display = lista.style.display === 'none' ? 'block' : 'none';
+}
